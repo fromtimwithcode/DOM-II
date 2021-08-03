1742292250503417339da5d693b1f7476c04896f
Tim Marchant <fromtimwithcode@gmail.com>
2021-08-03 16:20:40 -0500

    Mouseover/mouseout color swap on logo-heading

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+const logoHeading = document.querySelector('.logo-heading');
+
+// Change site title to red on mouseover, return to black on mouseout
+logoHeading.addEventListener('mouseover', (event) => {
+    event.target.style = 'color: red';
+});
+logoHeading.addEventListener('mouseout', (event) => {
+    event.target.style = 'color: black';
+});
